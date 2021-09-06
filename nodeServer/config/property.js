5610974f3ccf6272df97fafb36b896e46a101e75
Chung10Kr <coohg93@hanmail.net>
2021-09-06 22:27:00 +0900

    property.js commit

---
+++ nodeServer/config/property.js
@@ -0,0 +1,12 @@
+/*
+ * property
+ */
+
+module.exports = {
+
+	files_location:'D:/Project/project_log/files' ,
+	server_port:  '2000' ,
+	db_url:'mssql://sa:xxx@db.xxxx.co.kr/xxxx',
+	basic_location:'D:/Project/project/40.seah/SOURCE/PDA/PDA/nodeServer'
+
+};
